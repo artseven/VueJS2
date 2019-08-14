95280b2a3e8e6be54ad61445099599381a07083b
artseven <yabkevich.artsiom.09@gmail.com>
2019-08-14 14:16:48 -0400

    vue forms

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="hello">
     <div class="holder">
+      
+      <form @submit.prevent="addSkill">
+        <input type="text" placeholder="Enter a skill you have..." v-model="skill">
+        {{ skill }}
+      </form>
       <ul>
         <li v-for="(data, index) in skills" :key='index'>
           {{ data.skill }}
         </li>
       </ul>
+      
       <p>These are the skills that you possess.</p>
     </div>
   </div>
@@ -16,13 +22,17 @@ export default {
   name: 'Skills',
   data() {
     return {
+      skill: '',
       skills: [
         { "skill": "Vue.js"},
         { "skill": "Frontend Developer"},
-      ],
-      bgColor: 'yellow',
-      bgWidth: '100%',
-      bgHeight: '30px'
+      ]
+    }
+  },
+  methods: {
+    addSkill() {
+      this.skills.push({skill: this.skill})
+      this.skill = '';
     }
   }
 }
@@ -58,6 +68,15 @@ export default {
 
   .container {
     box-shadow: 0px 0px 40px lightgray;
+  }
+
+  input {
+    width: calc(100% - 40px);
+    border: 0;
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #323333;
+    color: #687F7F;
   }
 
 </style>
