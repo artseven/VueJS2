29981f0ee10ed5d992fe65467c515c1736d833a3
artseven <yabkevich.artsiom.09@gmail.com>
2019-08-14 14:39:56 -0400

    form validation

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -3,8 +3,8 @@
     <div class="holder">
       
       <form @submit.prevent="addSkill">
-        <input type="text" placeholder="Enter a skill you have..." v-model="skill">
-        {{ skill }}
+        <input type="text" placeholder="Enter a skill you have..." v-model="skill" v-validate="'min:5'" name="skill">
+        <p class="alert" v-if="errors.has('skill')">{{ errors.first('skill') }}</p>
       </form>
       <ul>
         <li v-for="(data, index) in skills" :key='index'>
@@ -22,6 +22,7 @@ export default {
   name: 'Skills',
   data() {
     return {
+      checked: false,
       skill: '',
       skills: [
         { "skill": "Vue.js"},
@@ -31,8 +32,14 @@ export default {
   },
   methods: {
     addSkill() {
-      this.skills.push({skill: this.skill})
-      this.skill = '';
+      this.$validator.validateAll().then((result) => {
+        if (result) {
+          this.skills.push({skill: this.skill})
+          this.skill = '';
+        } else {
+          console.log('Not valid');
+        }
+      })
     }
   }
 }
@@ -77,6 +84,14 @@ export default {
     font-size: 1.3em;
     background-color: #323333;
     color: #687F7F;
+  }
+
+  .alert {
+    background: #fdf2ce;
+    font-weight: bold;
+    display: inline-block;
+    padding: 5px;
+    margin-top: -20px;
   }
 
 </style>
